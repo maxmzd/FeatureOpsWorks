155fbcdd04e96bab328abed5c7721c16175718c3
maxmzd <b@maxmzd.com>
2015-03-24 18:48:12 -0700

    Try dev

--- vips/recipes/vips.rb
+++ vips/recipes/vips.rb
@@ -4,7 +4,7 @@ unless ::File.exists?("/usr/local/lib/libvips.so")
   bash "install vips" do
     cwd "/tmp"
     code <<-EOT
-    sudo apt-get -y install libvips libvips-tools
+    sudo apt-get -y install libvips-dev libvips-tools
     EOT
   end
 
